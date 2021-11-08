d83a3800961bcaa5912e1d111316af924deb9a49
Piotr <49452547+piotrgitt@users.noreply.github.com>
2021-11-08 01:22:42 +0100

    Poprawki, dodanie kalendarza

--- src/main/java/com/jsfcourse/haircutter/ReservationBB.java
+++ src/main/java/com/jsfcourse/haircutter/ReservationBB.java
@@ -1,29 +1,17 @@
 package com.jsfcourse.haircutter;
 
-import java.io.Serializable;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.application.FacesMessage;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
-import javax.faces.simplesecurity.RemoteClient;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.primefaces.PrimeFaces;
 import org.primefaces.event.SelectEvent;
-
-import java.text.SimpleDateFormat;
 
 import jsf.haircutter.dao.ReservationDAO;
 import jsf.haircutter.dao.ServiceDAO;
@@ -33,7 +21,7 @@ import jsf.haircutter.entities.Reservation;
 
 @Named
 @RequestScoped
-public class ReservationBB implements Serializable {
+public class ReservationBB  {
 
 	private static final String PAGE_THANKS = "/public/thanks?faces-redirect=true";
 	private static final String PAGE_CHOOSE_TIME = "/pages/userPages/temp_reservation?faces-redirect=true";
@@ -42,21 +30,12 @@ public class ReservationBB implements Serializable {
 	private boolean servicesRendered  =  true;
 	
 	//SETTING CALENDAR
-	private Date date;
-	private List<Date> invalidDates;
-	 private Date dateTimeDe;
-    private Date dateTimeMillis;
-    private List<Date> multi;
-    private List<Date> range;
-    private List<Integer> invalidDays;
-    private Date minDate;
-    private Date maxDate;
-    private Date minTime;
-    private Date maxTime;
-    private Date minDateTime;
-    private Date maxDateTime;
-    private Date dateDe;
-	
+	private LocalDate date;
+	private LocalDate date2;
+	 private LocalDate minDateTime;
+	 private LocalDate maxDateTime;
+	private boolean dateSet = false;
+   
 	@Inject
 	FacesContext ctx;
 
@@ -86,67 +65,21 @@ public class ReservationBB implements Serializable {
 		this.setServicesRendered(false);
 		
 
-		// 1. Pass object through session
-		// HttpSession session = (HttpSession) extcontext.getSession(true);
-		// session.setAttribute("person", person);
-
-		// 2. Pass object through flash
-		flash.put("reservation", reservation);
+		
 
 		return PAGE_STAY_AT_THE_SAME;
 	}
+	
+	
+	public void handleDateSelect(SelectEvent<LocalDate> event) {
+		System.out.println(dateSet);
+	    date = event.getObject();
+	    dateSet = true;
+	    
+	    //Add facesmessage
+	}
 	//PRIME FACES CALENDAR SETTINGS
-	 @PostConstruct
-	    public void init() {
-	        invalidDates = new ArrayList<>();
-	        Date today = new Date();
-	        invalidDates.add(today);
-	        long oneDay = 24 * 60 * 60 * 1000;
-	        for (int i = 0; i < 5; i++) {
-	            invalidDates.add(new Date(invalidDates.get(i).getTime() + oneDay));
-	        }
-
-	        invalidDays = new ArrayList<>();
-	        invalidDays.add(0);
-	        /* the first day of week is disabled */
-	        invalidDays.add(3);
-
-	        minDate = new Date(today.getTime() - (365 * oneDay));
-	        maxDate = new Date(today.getTime() + (365 * oneDay));
-
-	        Calendar tmp = Calendar.getInstance();
-	        tmp.set(Calendar.HOUR_OF_DAY, 9);
-	        tmp.set(Calendar.MINUTE, 0);
-	        tmp.set(Calendar.SECOND, 0);
-	        tmp.set(Calendar.MILLISECOND, 0);
-	        minTime = tmp.getTime();
-
-	        tmp = Calendar.getInstance();
-	        tmp.set(Calendar.HOUR_OF_DAY, 17);
-	        tmp.set(Calendar.MINUTE, 0);
-	        tmp.set(Calendar.SECOND, 0);
-	        tmp.set(Calendar.MILLISECOND, 0);
-	        maxTime = tmp.getTime();
-
-	        minDateTime = new Date(today.getTime() - (7 * oneDay));
-	        maxDateTime = new Date(today.getTime() + (7 * oneDay));
-
-	        dateDe = GregorianCalendar.getInstance().getTime();
-	        dateTimeDe = GregorianCalendar.getInstance().getTime();
-	        dateTimeMillis = GregorianCalendar.getInstance().getTime();
-	    }
-
-	    public void onDateSelect(SelectEvent<Date> event) {
-	        FacesContext facesContext = FacesContext.getCurrentInstance();
-	        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-	        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
-	    }
-
-	    public void click() {
-	        PrimeFaces.current().ajax().update("form:display");
-	        PrimeFaces.current().executeScript("PF('dlg').show()");
-	    }
-
+	
 
 	// GETTERS AND SETTERS
 
@@ -174,115 +107,49 @@ public class ReservationBB implements Serializable {
 		this.servicesRendered = servicesRendered;
 	}
 
-	public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-	public List<Date> getInvalidDates() {
-		return invalidDates;
+	public LocalDate getDate() {
+		return date;
 	}
 
-	public void setInvalidDates(List<Date> invalidDates) {
-		this.invalidDates = invalidDates;
+	public boolean isDateSet() {
+		return dateSet;
 	}
 
-	public Date getDateTimeDe() {
-		return dateTimeDe;
+	public void setDateSet(boolean dateSet) {
+		this.dateSet = dateSet;
 	}
 
-	public void setDateTimeDe(Date dateTimeDe) {
-		this.dateTimeDe = dateTimeDe;
+	public void setDate(LocalDate date) {
+		this.date = date;
 	}
 
-	public Date getDateTimeMillis() {
-		return dateTimeMillis;
+	public LocalDate getDate2() {
+		return date2;
 	}
 
-	public void setDateTimeMillis(Date dateTimeMillis) {
-		this.dateTimeMillis = dateTimeMillis;
+	public void setDate2(LocalDate date2) {
+		this.date2 = date2;
 	}
 
-	public List<Date> getMulti() {
-		return multi;
-	}
-
-	public void setMulti(List<Date> multi) {
-		this.multi = multi;
-	}
-
-	public List<Date> getRange() {
-		return range;
-	}
-
-	public void setRange(List<Date> range) {
-		this.range = range;
-	}
-
-	public List<Integer> getInvalidDays() {
-		return invalidDays;
-	}
-
-	public void setInvalidDays(List<Integer> invalidDays) {
-		this.invalidDays = invalidDays;
-	}
-
-	public Date getMinDate() {
-		return minDate;
-	}
-
-	public void setMinDate(Date minDate) {
-		this.minDate = minDate;
-	}
-
-	public Date getMaxDate() {
-		return maxDate;
-	}
-
-	public void setMaxDate(Date maxDate) {
-		this.maxDate = maxDate;
-	}
-
-	public Date getMinTime() {
-		return minTime;
-	}
-
-	public void setMinTime(Date minTime) {
-		this.minTime = minTime;
-	}
-
-	public Date getMaxTime() {
-		return maxTime;
-	}
-
-	public void setMaxTime(Date maxTime) {
-		this.maxTime = maxTime;
-	}
-
-	public Date getMinDateTime() {
+	public LocalDate getMinDateTime() {
 		return minDateTime;
 	}
 
-	public void setMinDateTime(Date minDateTime) {
+	public void setMinDateTime(LocalDate minDateTime) {
 		this.minDateTime = minDateTime;
 	}
 
-	public Date getMaxDateTime() {
+	public LocalDate getMaxDateTime() {
 		return maxDateTime;
 	}
 
-	public void setMaxDateTime(Date maxDateTime) {
+	public void setMaxDateTime(LocalDate maxDateTime) {
 		this.maxDateTime = maxDateTime;
 	}
 
-	public Date getDateDe() {
-		return dateDe;
-	}
+	
 
-	public void setDateDe(Date dateDe) {
-		this.dateDe = dateDe;
-	}
+	
+
+	
 }
